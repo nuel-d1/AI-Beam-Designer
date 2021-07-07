5cd70af17516510ed21510599564a2aa19811ee6
Emmanuel Dickson <emmanueldickson5@gmail.com>
2021-07-07 22:58:22 +0100

    incorporated streamlit

--- training.py
+++ training.py
@@ -6,26 +6,27 @@ import matplotlib.pyplot as plt
 from torch import optim, nn
 from model import Net
 from dataset import feature_dataset
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 
 # read entire data from csv file
-dataset = pd.read_csv('data/data.csv')
+train_dataset = pd.read_csv('data/train_data.csv')
+test_dataset = pd.read_csv('data/test_data.csv')
 
 # split dataset into training and testing sets &
 # load into custom dataset class
-train = feature_dataset(dataset[:90])
-test = feature_dataset(dataset[90:])
+train = feature_dataset(train_dataset)
+test = feature_dataset(test_dataset)
 train_loader = torch.utils.data.DataLoader(train, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test)
 
 model = Net()
-learning_rate = 0.01
+learning_rate = 0.004
 criterion = nn.BCELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 
 def training():
-    writer = SummaryWriter()
+    # writer = SummaryWriter()
     epochs = 500
 
     for epoch in range(epochs):
@@ -37,31 +38,34 @@ def training():
             train_loss.append(loss.item())
             tr_loss = np.array(train_loss)
 
-            for i, val in enumerate(tr_loss):
-                writer.add_scalar(
-                    'Loss/train', scalar_value=val, global_step=i)
+            # for i, val in enumerate(tr_loss):
+            # writer.add_scalar(
+            #     'Loss/train', scalar_value=val, global_step=i)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         if(epoch % 100 == 0):
-            # Testing
-            with torch.no_grad():
-                test_loss = []
-                
-                for features, labels in test_loader:
-                    output = model(features)
-                    loss = criterion(output, labels)
-                    test_loss.append(loss.item())
-                    te_loss = np.array(test_loss)
-                    
-                    for i, val in enumerate(te_loss):
-                        writer.add_scalar(
-                            'Loss/test', scalar_value=val, global_step=i)
 
             print('Epoch {}/{} | Training loss: {:.4f} | Test loss: {:.4f}'.format(
                 epoch, epochs, sum(train_loss), sum(test_loss)))
+
+
+def testing():
+    # Testing
+    with torch.no_grad():
+        test_loss = []
+
+        for features, labels in test_loader:
+            output = model(features)
+            loss = criterion(output, labels)
+            test_loss.append(loss.item())
+            te_loss = np.array(test_loss)
+
+            # for i, val in enumerate(te_loss):
+            #     writer.add_scalar(
+            #         'Loss/test', scalar_value=val, global_step=i)
 
 
 def make_plot(train_loss, test_loss):
@@ -72,4 +76,8 @@ def make_plot(train_loss, test_loss):
     plt.show()
 
 
+# begin training
 training()
+
+# Save the model state
+torch.save(model.state_dict(), 'designer_state.pth')
